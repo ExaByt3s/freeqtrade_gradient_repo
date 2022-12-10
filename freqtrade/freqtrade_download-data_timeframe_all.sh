3c9e452824ea9f1766e04e97b06d27cd03318fdc
Inoue Hiroo <one@localhost>
2022-12-10 16:10:33 +0900

    WIP

--- freqtrade/freqtrade_download-data_timeframe_all.sh
+++ freqtrade/freqtrade_download-data_timeframe_all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 TIMERANGE='20000101-20221201'
 # 1m 3m 5m 15m 30m 1h 2h 4h 6h 8h 12h 1d 3d 1w 1M
-TIMEFRAME='5m'
+TIMEFRAME='5m 15m 30m 1h 2h 4h 6h 8h 12h 1d 3d 1w 1M'
 
-freqtrade download-data --timeframes ${TIMEFRAME} --timerange "${TIMERANGE}"
-#  --trading-mode futures -p BTC/USDT ETH/USDT
+freqtrade download-data --timeframes ${TIMEFRAME} --timerange "${TIMERANGE}" -p BTC/USDT ETH/USDT
+#  --trading-mode futures
