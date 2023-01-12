0953a3534d4bbd631ca307e72b8689ee45368fb6
ultrahiroo <ultrahiroo2000@yahoo.co.jp>
2023-01-12 16:08:38 +0000

    WIP

--- mctx/install_jax.sh
+++ mctx/install_jax.sh
@@ -3,5 +3,4 @@ cd jax/
 sudo apt install g++ python python3-dev
 pip install numpy wheel
 python build/build.py
-# python build/build.py --bazel_options=--override_repository=org_tensorflow=https://github.com/graphcore/tensorflow
 pip install dist/*.whl
