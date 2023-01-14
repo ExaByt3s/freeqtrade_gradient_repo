a3fbe2dfc1f0eab577d68bd51b7e60b741074102
ultrahiroo <ultrahiroo2000@yahoo.co.jp>
2023-01-14 17:17:39 +0000

    WIP

--- mctx/install_jax_ipu.sh
+++ mctx/install_jax_ipu.sh
@@ -1,7 +1,8 @@
-git clone https://github.com/google/jax --depth 1
+# git clone https://github.com/google/jax --depth 1
 cd jax/
-sudo apt install g++ python python3-dev
-pip install numpy wheel
-git clone https://github.com/graphcore/tensorflow --branch '2.6/sdk-release-3.1' --depth 1
-python build/build.py --bazel_options=--override_repository=org_tensorflow=./tensorflow
+# sudo apt install g++ python python3-dev
+# pip install numpy wheel
+# git clone https://github.com/graphcore/tensorflow --branch 'r2.6/sdk-release-2.6' --depth 1
+python build/build.py --bazel_options=--override_repository=org_tensorflow=${PWD}/tensorflow
 # pip install dist/*.whl
+git clone https://github.com/graphcore/upstream-patches-awf-google-jax
---
+++ mctx/install_tensorflow.sh
@@ -0,0 +1,3 @@
+wget https://github.com/bazelbuild/bazel/releases/download/3.7.2/bazel_3.7.2-linux-x86_64.deb
+apt install ./bazel_3.7.2-linux-x86_64.deb
+./configure
